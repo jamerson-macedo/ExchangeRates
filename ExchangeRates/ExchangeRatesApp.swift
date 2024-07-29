dab92dda73fceb3b192aa43e4199b6ccb81bdc88
jamerson-macedo <jamerson_macedo@hotmail.com>
2024-07-29 09:54:25 -0300

    Initial Commit

---
+++ ExchangeRates/ExchangeRatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeRatesApp.swift
+//  ExchangeRates
+//
+//  Created by Jamerson Macedo on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeRatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
